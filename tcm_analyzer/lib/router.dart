6085aaa0e946f1ab2d5005ef832dbc4edc295b10
Benedictus Kent <benedictuskent123@gmail.com>
2021-05-02 12:23:54 +0800

    add routers to main

---
+++ tcm_analyzer/lib/router.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:tcm_analyzer/home.dart';
+import 'package:tcm_analyzer/info.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => HomePage());
+      case '/info':
+        return MaterialPageRoute(builder: (_) => InfoPage(data: args));
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Error"),
+        ),
+        body: Center(
+          child: Text("ERROR"),
+        ),
+      );
+    });
+  }
+}
