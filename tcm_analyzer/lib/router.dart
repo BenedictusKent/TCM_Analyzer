f4bb123454596671e16cefb35cea9a046398c553
Benedictus Kent <benedictuskent123@gmail.com>
2021-05-03 03:19:31 +0800

    add camera and view routes

--- tcm_analyzer/lib/router.dart
+++ tcm_analyzer/lib/router.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:tcm_analyzer/home.dart';
 import 'package:tcm_analyzer/info.dart';
+import 'package:tcm_analyzer/camera.dart';
+import 'package:tcm_analyzer/view.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -10,6 +12,10 @@ class RouteGenerator {
         return MaterialPageRoute(builder: (_) => HomePage());
       case '/info':
         return MaterialPageRoute(builder: (_) => InfoPage(data: args));
+      case '/camera':
+        return MaterialPageRoute(builder: (_) => CameraScreen());
+      case '/view':
+        return MaterialPageRoute(builder: (_) => ViewPage(image: args));
       default:
         return _errorRoute();
     }
