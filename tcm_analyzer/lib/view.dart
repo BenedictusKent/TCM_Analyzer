84bb9eb891a7c6836671fff71869c05fea175e7e
Benedictus Kent <benedictuskent123@gmail.com>
2021-05-09 16:31:18 +0800

    add crop image feature

--- tcm_analyzer/lib/view.dart
+++ tcm_analyzer/lib/view.dart
@@ -1,5 +1,10 @@
 import 'dart:io';
+import 'dart:ui' as ui;
 import 'package:camera/camera.dart';
+import 'package:image_size_getter/file_input.dart';
+import 'package:flutter_native_image/flutter_native_image.dart';
+import 'package:image_size_getter/image_size_getter.dart' as isg;
+import 'package:image/image.dart' as img;
 import 'package:flutter/material.dart';
 
 class ViewPage extends StatefulWidget {
@@ -14,6 +19,40 @@ class ViewPage extends StatefulWidget {
 }
 
 class _ViewPageState extends State<ViewPage> {
+  Future<ui.Image> thumbnail;
+
+  Future<ui.Image> getUiImage(int height, int width) async {
+    img.Image dcd = img.decodeImage(File(widget.image.path).readAsBytesSync());
+    img.Image rsz = img.copyResize(dcd, height: height, width: width);
+    ui.Codec codec = await ui.instantiateImageCodec(img.encodePng(rsz));
+    ui.FrameInfo frameInfo = await codec.getNextFrame();
+    return frameInfo.image;
+  }
+
+  File imgCropped;
+  Future<File> cropImage(int height, int width) async {
+    File convert = File(widget.image.path);
+    var dim = isg.ImageSizeGetter.getSize(FileInput(convert));
+    var high = dim.height;
+    var wide = dim.width;
+    int x = ((wide / 2) - (width / 2)).round();
+    int y = ((high / 2) - (height / 2)).round();
+    File croppedFile = await FlutterNativeImage.cropImage(
+        widget.image.path, x, y, width, height);
+    return croppedFile;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    cropImage(395, 395).then((value) {
+      setState(() {
+        imgCropped = value;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +66,9 @@ class _ViewPageState extends State<ViewPage> {
             padding: EdgeInsets.only(top: 10.0),
             height: MediaQuery.of(context).size.height * 2 / 3,
             width: MediaQuery.of(context).size.width,
-            child: Image.file(File(widget.image.path)),
+            child: imgCropped != null
+                ? Image.file(File(imgCropped.path))
+                : Text("It's null..."),
           ),
           Padding(
             padding: EdgeInsets.all(40),
@@ -38,9 +79,7 @@ class _ViewPageState extends State<ViewPage> {
               ),
               style: ElevatedButton.styleFrom(
                   primary: Color(0xFE2B3F87), minimumSize: Size(300, 50)),
-              onPressed: () {
-                print("success");
-              },
+              onPressed: () {},
             ),
           )
         ],
