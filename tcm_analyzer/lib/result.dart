abda986c49dcf5242ce242211f29479d7cb1a809
Gavin Vivaldy <gav.vivaldy@gmail.com>
2021-05-14 02:36:37 +0800

    implement herb list view for prediction result page

--- tcm_analyzer/lib/result.dart
+++ tcm_analyzer/lib/result.dart
@@ -26,9 +26,58 @@ class _ResultPageState extends State<ResultPage> {
             itemCount: widget.responseData.length,
             itemBuilder: (context, index) {
               final item = widget.responseData[index];
-              return ListTile(
-                title: Text(item),
-              );
+              return GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/info',
+                                arguments: item["engName"]);
+                          },
+                          child: Container(
+                              height: 130,
+                              margin: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 10),
+                              decoration: BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(20.0)),
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: Colors.black.withAlpha(100),
+                                        blurRadius: 10.0),
+                                  ]),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 20.0, vertical: 10),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Image.asset(item["thumbnail"],
+                                        height: 100, width: 100),
+                                    Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 10)),
+                                    Expanded(
+                                        child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Text(
+                                          item["engName"],
+                                          style: const TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text(
+                                          item["cnName"],
+                                          style: const TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ],
+                                    )),
+                                  ],
+                                ),
+                              )));
             },
           )
         ],
