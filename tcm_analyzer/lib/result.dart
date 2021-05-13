e912ca287747c7a61ef51641420896b172b28a1a
Benedictus Kent <benedictuskent123@gmail.com>
2021-05-14 02:02:40 +0800

    remove unused imports in result.dart

--- tcm_analyzer/lib/result.dart
+++ tcm_analyzer/lib/result.dart
@@ -1,16 +1,4 @@
-import 'dart:io';
-import 'dart:async';
-import 'dart:ui' as ui;
-import 'dart:convert';
-import 'package:camera/camera.dart';
-import 'package:image_size_getter/file_input.dart';
-import 'package:flutter_native_image/flutter_native_image.dart';
-import 'package:image_size_getter/image_size_getter.dart' as isg;
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:http_parser/http_parser.dart';
-import 'package:mime/mime.dart';
-import 'package:path/path.dart';
 
 class ResultPage extends StatefulWidget {
   final List responseData;
